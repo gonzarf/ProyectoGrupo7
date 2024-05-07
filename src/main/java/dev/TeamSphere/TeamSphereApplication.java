a792a57a14cf350fda0a06ae5c8bea30140f590c
evagmezz <eva.gomez@alumno.iesluisvives.org>
2024-05-07 11:29:49 +0200

    configuration

---
+++ src/main/java/dev/TeamSphere/TeamSphereApplication.java
@@ -0,0 +1,13 @@
+package dev.TeamSphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamSphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamSphereApplication.class, args);
+	}
+
+}
