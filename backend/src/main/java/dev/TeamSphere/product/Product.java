1b13c711941098aa7d6e755160c4293371c1baaa
DoctorProgamador <n.suarez.ruiz04@gmail.com>
2024-05-08 17:11:42 +0200

    creacion dto product

--- backend/src/main/java/dev/TeamSphere/product/Product.java
+++ backend/src/main/java/dev/TeamSphere/product/Product.java
@@ -1,39 +1,37 @@
 package dev.TeamSphere.product;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.UUID;
+
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
 @EntityListeners(AuditingEntityListener.class)
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Product {
 
-    public static final String IMAGE_DEFAULT = "https://www.devgraph.com/wp-content/uploads/2022/01/tumbnail-placeholder-image.jpeg";
 
-    @UUID
+    @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     @Column(nullable = false)
-    @NotBlank(message = "The name is required")
     private String name;
 
     @Column(nullable = false)
-    @NotBlank(message = "The price is required")
-    private double price;
+    private Double price;
 
     @Column(nullable = false)
-    @NotBlank(message = "The description is required")
     private String description;
 
     @CreatedDate
@@ -41,8 +39,7 @@ public class Product {
     private LocalDateTime date = LocalDateTime.now();
 
     @Column(nullable = false)
-    @NotBlank
-    private String image = IMAGE_DEFAULT;
+    private String image;
 
     //private UUID id_comprador;
 
