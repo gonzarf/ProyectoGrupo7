23562b54e98d64fb99e887dc9962a4589b8e9cdf
javierrodriguez7 <169168954+javierrodriguez7@users.noreply.github.com>
2024-05-08 17:06:57 +0200

    login terminado

--- frontend/TeamSphere/src/app/login/login.component.ts
+++ frontend/TeamSphere/src/app/login/login.component.ts
@@ -1,13 +1,50 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { LoginServices } from '../../Services/login.services';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Usuario } from './usuario.model';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterLink, HttpClientModule, FormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent{
+  constructor(private service:LoginServices){
+  }
+
+  email:string = "";
+  
+  usuario:Promise<Usuario> | undefined;
+  
+  
+  login(): void {
+    this.usuario= this.service.login(this.email);
+    console.log(typeof this.usuario);
+    
+    console.log(this.usuario);
+    
+    this.usuario!.then(
+      (result) =>{
+        if(result.email==""){
+          alert("tu madre");
+          console.log("no va");
+          
+        }else{
+          alert("mi padre")
+          console.log("va");
+          
+        }
+      }
+    );
+  }
+
+
 
   showSignUp(): void {
     const container = document.querySelector('.container') as HTMLDivElement;
---
+++ frontend/TeamSphere/src/app/login/usuario.model.ts
@@ -0,0 +1,8 @@
+export interface Usuario{
+    id: string
+    name: string
+    lastName: string
+    email: string
+    password: string
+    phone: string
+  }
