f5efa272dae712da051dcbf512cc71e77a2dd852
IbazRob <IgnacioIbanezRobles@protonmail.com>
2024-05-10 11:16:11 +0200

    Componente notia

--- frontend/TeamSphere/src/app/home/home.component.ts
+++ frontend/TeamSphere/src/app/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { SideBarComponent } from "../side-bar/side-bar.component";
+import { NoticiaComponent } from "../noticia/noticia.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [SideBarComponent]
+    imports: [SideBarComponent, NoticiaComponent]
 })
 export class HomeComponent {
 
