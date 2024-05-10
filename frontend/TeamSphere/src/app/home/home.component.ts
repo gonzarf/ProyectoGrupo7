76be2869d3109532d0fe1716078c13e89ec6de3f
IbazRob <IgnacioIbanezRobles@protonmail.com>
2024-05-10 10:28:50 +0200

    Home, sidebar

--- frontend/TeamSphere/src/app/home/home.component.ts
+++ frontend/TeamSphere/src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { SideBarComponent } from "../side-bar/side-bar.component";
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css',
+    imports: [SideBarComponent]
 })
 export class HomeComponent {
 
