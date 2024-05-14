44583081cf67b1c7aa682c8669cf54343b7e80b0
IbazRob <IgnacioIbanezRobles@protonmail.com>
2024-05-14 12:31:42 +0200

    Revert "Navbar"
    This reverts commit c6a35d7d3037d50bba5d0219dc222e4020a7a11f.

--- frontend/TeamSphere/src/app/compra-venta/compra-venta.component.ts
+++ frontend/TeamSphere/src/app/compra-venta/compra-venta.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
 import { SideBarComponent } from "../side-bar/side-bar.component";
 import { ItemComponent } from "../item/item.component";
-import { NavbarComponent } from "../navbar/navbar.component";
 
 @Component({
     selector: 'app-compra-venta',
     standalone: true,
     templateUrl: './compra-venta.component.html',
     styleUrl: './compra-venta.component.css',
-    imports: [SideBarComponent, ItemComponent, NavbarComponent]
+    imports: [SideBarComponent, ItemComponent]
 })
 export class CompraVentaComponent {
-    title = "Compra / Venta";
+
 }
