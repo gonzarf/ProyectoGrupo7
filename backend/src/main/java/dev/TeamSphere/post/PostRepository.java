75f45cf6be88e97350f5a966faf5beabc53c0033
carloosbravo <132547371+carloosbravo@users.noreply.github.com>
2024-05-14 13:49:43 +0200

    repository de post añadido

---
+++ backend/src/main/java/dev/TeamSphere/post/PostRepository.java
@@ -0,0 +1,8 @@
+package dev.TeamSphere.post;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface PostRepository extends JpaRepository<Post, UUID> {
+}
