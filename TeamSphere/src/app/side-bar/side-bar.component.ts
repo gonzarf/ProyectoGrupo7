64c9f36a89950bd922f11172c51159213e2cc2e0
GuillermoMuñozManzanal <58306812+Guillermo3918@users.noreply.github.com>
2024-05-14 14:36:54 +0200

    Add files via upload

---
+++ TeamSphere/src/app/side-bar/side-bar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-side-bar',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './side-bar.component.html',
+  styleUrl: './side-bar.component.css'
+})
+export class SideBarComponent {
+
+}
