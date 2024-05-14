6f1d967c00722261240c79795f291e86570690d1
IbazRob <IgnacioIbanezRobles@protonmail.com>
2024-05-14 09:59:29 +0200

    link

--- frontend/TeamSphere/src/app/noticia/noticia.component.ts
+++ frontend/TeamSphere/src/app/noticia/noticia.component.ts
@@ -8,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './noticia.component.css'
 })
 export class NoticiaComponent {
-
+  id: string = "";
+  tittle: string = "";
+  desc: string = "";
+  img: string = "";
 }
