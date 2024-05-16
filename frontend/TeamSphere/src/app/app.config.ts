5cd83ffe5c0bfffc25ce2246687ccdecc1db8d23
IbazRob <IgnacioIbanezRobles@protonmail.com>
2024-05-16 14:21:03 +0200

    update

--- frontend/TeamSphere/src/app/app.config.ts
+++ frontend/TeamSphere/src/app/app.config.ts
@@ -7,5 +7,5 @@ import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(), provideAnimationsAsync(),]
+  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(),]
 };
