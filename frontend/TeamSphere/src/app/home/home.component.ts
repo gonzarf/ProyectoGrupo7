704577faa676d45fbcfd70ca54458c79fc7b583e
gonzarf <gonzarf21@gmail.com>
2024-05-17 10:12:18 +0200

    conexion back con home

--- frontend/TeamSphere/src/app/home/home.component.ts
+++ frontend/TeamSphere/src/app/home/home.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SideBarComponent } from "../side-bar/side-bar.component";
 import { NavbarComponent } from "../navbar/navbar.component";
 import { NoticiaComponent } from "../noticia/noticia.component";
 import { RouterLink } from '@angular/router';
 import { HomeServices } from '../../Services/home.services';
-import { Noticia } from '../noticia/noticia.model';
 import { NgForOf } from '@angular/common';
 import { SortBarComponent } from "../sort-bar/sort-bar.component";
+import { Noticia } from '../noticia/noticia.model';
 
 
 @Component({
@@ -16,6 +16,23 @@ import { SortBarComponent } from "../sort-bar/sort-bar.component";
     styleUrl: './home.component.css',
     imports: [SideBarComponent, NoticiaComponent, RouterLink, NoticiaComponent, NgForOf, NavbarComponent, SortBarComponent]
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+    constructor(private service:HomeServices){
+
+    }
+
     title = "Home";
+
+    ngOnInit(): void {
+        this.loadNews()
+    }
+
+    noticias:Array<Noticia> = new Array<Noticia>;
+
+    loadNews(): void {
+        this.service.loadNews().subscribe(data =>{
+            this.noticias = data
+        })
+      }
 }
