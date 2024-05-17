1e11b298e97ce51ed7aa12265beadacf1858fae1
javierrodriguez7 <169168954+javierrodriguez7@users.noreply.github.com>
2024-05-17 14:00:52 +0200

    boton mas pop-up finalizado

--- frontend/TeamSphere/src/app/afterwork/afterwork.component.ts
+++ frontend/TeamSphere/src/app/afterwork/afterwork.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { SideBarComponent } from "../side-bar/side-bar.component";
 import { NavbarComponent } from '../navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 
 
@@ -9,8 +11,22 @@ import { NavbarComponent } from '../navbar/navbar.component';
     standalone: true,
     templateUrl: './afterwork.component.html',
     styleUrl: './afterwork.component.css',
-    imports: [SideBarComponent, NavbarComponent]
+    imports: [SideBarComponent, NavbarComponent, FormsModule, CommonModule]
 })
 export class AfterworkComponent {
     title= "AfterWork";
+  isFormVisible = false;
+  formData = {
+    field1: '',
+    field2: '',
+    field3: '',
+    field4: ''
+  };
+
+  toggleForm() {
+    this.isFormVisible = !this.isFormVisible;
+  }
+  cancel() {
+    this.isFormVisible = false;
+  }
 }
