b612b87d8f1dc6794ba7d5a7e2277bb97311a1c4
GuillermoMuñozManzanal <58306812+Guillermo3918@users.noreply.github.com>
2024-05-22 09:54:46 +0200

    botón apuntarse/desapuntarse funcional

--- frontend/TeamSphere/src/app/desplazamientos-item/desplazamientos-item.component.ts
+++ frontend/TeamSphere/src/app/desplazamientos-item/desplazamientos-item.component.ts
@@ -1,24 +1,43 @@
-import { Component } from '@angular/core';
+import { NgClass, NgStyle } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-desplazamientos-item',
   standalone: true,
-  imports: [],
+  imports: [NgStyle,NgClass],
   templateUrl: './desplazamientos-item.component.html',
   styleUrl: './desplazamientos-item.component.css'
 })
 export class DesplazamientosItemComponent {
 
   espacios = 0;
-  // btnApuntarse = document.getElementsByClassName("likes_text");
-
+  btnApuntarse = "fill";
+  btnDesapuntarse = "fill";
+  
 
   Apuntarse(){
     
     if(this.espacios < 4){
 
       this.espacios++;
+      this.btnApuntarse = "none";
+      this.btnDesapuntarse = "fill"
       
+    }
+   
+    
+  }
+  Desapuntarse(){
+    
+    this.espacios--;
+
+
+    if(this.espacios < 4){
+
+      
+      this.btnDesapuntarse = "none";
+      this.btnApuntarse = "fill";
+       
     }
    
     
--- frontend/TeamSphere/src/app/desplazamientos/desplazamientos.component.ts
+++ frontend/TeamSphere/src/app/desplazamientos/desplazamientos.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { SideBarComponent } from "../side-bar/side-bar.component";
 import { NavbarComponent } from "../navbar/navbar.component";
 import { DesplazamientosItemComponent } from '../desplazamientos-item/desplazamientos-item.component';
+import { NgStyle } from '@angular/common';
 
 @Component({
     selector: 'app-desplazamientos',
     standalone: true,
     templateUrl: './desplazamientos.component.html',
     styleUrl: './desplazamientos.component.css',
-    imports: [SideBarComponent, NavbarComponent, DesplazamientosItemComponent]
+    imports: [SideBarComponent, NavbarComponent, DesplazamientosItemComponent,NgStyle]
 })
 export class DesplazamientosComponent {
-    title = "Desplazamientos"
+    title = "Desplazamientos";
+    
 }
