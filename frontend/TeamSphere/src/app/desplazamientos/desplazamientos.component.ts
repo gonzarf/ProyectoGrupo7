80cdbb67a5c482f7049ef3b3aeaf2ab3ff42c1c8
javierrodriguez7 <169168954+javierrodriguez7@users.noreply.github.com>
2024-05-23 15:40:16 +0200

    post desplazmiento conectado con el  backend creado

--- frontend/TeamSphere/src/app/desplazamientos/desplazamientos.component.ts
+++ frontend/TeamSphere/src/app/desplazamientos/desplazamientos.component.ts
@@ -2,16 +2,66 @@ import { Component } from '@angular/core';
 import { SideBarComponent } from "../side-bar/side-bar.component";
 import { NavbarComponent } from "../navbar/navbar.component";
 import { DesplazamientosItemComponent } from '../desplazamientos-item/desplazamientos-item.component';
-import { NgStyle } from '@angular/common';
+import { CommonModule, NgStyle } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Desplazamiento } from './desplazamiento.model';
+import { DisplacementServices } from '../../Services/displacement.service';
 
 @Component({
     selector: 'app-desplazamientos',
     standalone: true,
     templateUrl: './desplazamientos.component.html',
     styleUrl: './desplazamientos.component.css',
-    imports: [SideBarComponent, NavbarComponent, DesplazamientosItemComponent,NgStyle]
+    imports: [SideBarComponent, NavbarComponent, DesplazamientosItemComponent,NgStyle, CommonModule, FormsModule]
 })
 export class DesplazamientosComponent {
+    constructor(private service: DisplacementServices) {}
+
     title = "Desplazamientos";
+
+    ngOnInit(): void {
+        this.loadDisplacement();
+      }
+
+    isFormVisible = false;
+
+    titulo = "";
+    description="";
+    type = "desplazamiento";
+    fechaSalida = "";
+
+formData = {
+        field1: '',
+        field2: '',
+        field3: '',
+        field4: ''
+      };
+
+    desplazamientos: Array<Desplazamiento> = new Array<Desplazamiento>();
+
     
+
+      loadDisplacement(): void {
+        this.service.loadDisplacement().subscribe((data) => {
+          this.desplazamientos = data;
+        });
+      }
+
+    toggleForm() {
+        this.isFormVisible = !this.isFormVisible;
+      }
+      cancel() {
+        this.isFormVisible = false;
+      }
+
+      createDisplacement(){
+        let desplazamiento : Desplazamiento = {
+          title: this.titulo,
+          description: this.fechaSalida + ", " + this.title + ", " + this.description,
+          type: this.type
+        }
+    
+        this.service.createDisplacement(desplazamiento);
+        window.location.reload();
+      }
 }
