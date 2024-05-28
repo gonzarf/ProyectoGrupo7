dcd954d0525e7776b801935e2cce4b2ee237e3fe
carloosbravo <132547371+carloosbravo@users.noreply.github.com>
2024-05-28 10:18:21 +0200

    buscador de post por titulo

--- backend/src/main/java/dev/TeamSphere/post/PostController.java
+++ backend/src/main/java/dev/TeamSphere/post/PostController.java
@@ -98,5 +98,9 @@ public class PostController {
 
     }
 
+    @GetMapping("/search")
+    public List<Post> searchPosts(@RequestParam String query) {
+        return postService.searchPosts(query);
+    }
 
 }
--- backend/src/main/java/dev/TeamSphere/post/PostRepository.java
+++ backend/src/main/java/dev/TeamSphere/post/PostRepository.java
@@ -2,9 +2,15 @@ package dev.TeamSphere.post;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface PostRepository extends JpaRepository<Post, UUID> {
     Optional<Post> findById(UUID id);
+
+    List<Post> findByTitleContainingIgnoreCase(String title);
+
+
+
 }
